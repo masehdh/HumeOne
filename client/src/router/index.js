336e378af4b989eee356d503c9fb713c20349bd1
Maseh Hadaf <maseh46@gmail.com>
2021-08-11 11:17:02 -0400

    c

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,71 +1,76 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory } from "vue-router";
 
 const routes = [
   {
-    path: '/',
-    name: 'Home',
-    component: () =>
-      import(/* webpackChunkName: "home" */ "@/views/Home.vue"),
+    path: "/",
+    name: "Home",
+    component: () => import(/* webpackChunkName: "home" */ "@/views/Home.vue"),
     meta: {
-      title: "Home"
-    }
+      title: "Home",
+    },
   },
   {
-    path: '/sign-up/',
-    name: 'Sign Up',
+    path: "/sign-up/",
+    name: "Sign Up",
     props: true,
     component: () =>
       import(/* webpackChunkName: "sign-up" */ "@/views/SignUp.vue"),
-      meta: {
-        title: "Sign Up"
-      }
+    meta: {
+      title: "Sign Up",
+    },
   },
   {
-    path: '/event-registration/',
-    name: 'Event Registration',
+    path: "/event-registration/",
+    name: "Event Registration",
     component: () =>
-      import(/* webpackChunkName: "register" */ "@/views/EventRegistration.vue"),
-      meta: {
-        title: "Event Registration"
-      }
+      import(
+        /* webpackChunkName: "register" */ "@/views/EventRegistration.vue"
+      ),
+    meta: {
+      title: "Event Registration",
+    },
   },
   {
-    path: '/event-registration-confirmation/',
-    name: 'Event Registration Confirmation',
+    path: "/event-registration-confirmation/",
+    name: "Event Registration Confirmation",
     props: true,
     component: () =>
-      import(/* webpackChunkName: "event registration confirmation" */ "@/views/EventRegistrationConfirmation.vue"),
-      meta: {
-        title: "Event Registration Confirmation"
-      }
+      import(
+        /* webpackChunkName: "event registration confirmation" */ "@/views/EventRegistrationConfirmation.vue"
+      ),
+    meta: {
+      title: "Event Registration Confirmation",
+    },
   },
   {
-    path: '/sign-up-confirmation/',
-    name: 'Sign Up Confirmation',
+    path: "/sign-up-confirmation/",
+    name: "Sign Up Confirmation",
     component: () =>
-      import(/* webpackChunkName: "sign up confirmation" */ "@/views/SignUpConfirmation.vue"),
-      meta: {
-        title: "Sign Up Confirmation"
-      }
+      import(
+        /* webpackChunkName: "sign up confirmation" */ "@/views/SignUpConfirmation.vue"
+      ),
+    meta: {
+      title: "Sign Up Confirmation",
+    },
   },
   {
-    path: '/:pathMatch(.*)*',
-    name: '404',
+    path: "/:pathMatch(.*)*",
+    name: "404",
     component: () => import(/* webpackChunkName: "404" */ "../views/404.vue"),
     meta: {
-      title: '404',
+      title: "404",
       requiresAuth: false,
-    }
-  }
-]
+    },
+  },
+];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes: routes,
   scrollBehavior() {
     // always scroll to top
-    return { top: 0 }
+    return { top: 0 };
   },
-})
+});
 
-export default router
+export default router;
