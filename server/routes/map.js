30e3ae20bc796062c9b6483d09b33ac6ca28d5c0
Shareef Hadid <shareefhadid@gmail.com>
2021-09-23 17:51:09 -0400

    removed mapbox

--- server/routes/map.js
+++ server/routes/map.js
@@ -4,31 +4,6 @@ const citiesGeodata = require("../../resources/cities-geodata.json");
 
 const router = express.Router();
 
-// router.post("/auto-complete", (req, res) => {
-//   try {
-//     const baseURL = "https://api.mapbox.com/geocoding/v5/mapbox.places/"
-//     const queryURL = `${req.body.input}.json?types=address&proximity=-79.347015,43.651070&country=CA&access_token=${process.env.MAPBOX_API_KEY}`
-
-//     axios.get(baseURL + queryURL)
-//       .then((output => {
-//         res
-//           .status(200)
-//           .json({
-//             code: 200,
-//             message: "",
-//             output: output.data.features,
-//           });
-//       }))
-//       .catch((error) =>
-//         console.log("Error from Maps API:", error)
-//       )
-//   } catch (error) {
-//     return res
-//       .status(400)
-//       .json({ code: error.code, message: error.message, stack: error.stack });
-//   }
-// })
-
 router.post("/auto-complete", (req, res) => {
   try {
     let possibleCities = []
