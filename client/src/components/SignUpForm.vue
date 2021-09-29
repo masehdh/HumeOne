d144557a6fa7645e7a383f4a54f61d376a94d9c6
Maseh Hadaf <maseh46@gmail.com>
2021-09-29 15:31:21 -0400

    multistage

--- client/src/components/SignUpForm.vue
+++ client/src/components/SignUpForm.vue
@@ -6,246 +6,205 @@
         Complete the form below to start receiving invites
       </p>
     </div>
-
-    <div class="md:flex md:flex-row">
-      <!-- First Name Field -->
-      <div class="form-control md:mr-3">
-        <span class="p-float-label">
-          <InputText
-            type="text"
-            id="first-name"
-            v-model="firstName"
-            v-tooltip.bottom.focus="'Your name will be displayed publicly'"
-            :class="{
-              'p-invalid': validationMessages.hasOwnProperty('firstName')
-            }"
-            class="w-20rem "
-            @blur="sendSignUpInfo()"
-          />
-          <label for="first-name">First Name</label>
-        </span>
-        <div
-          v-for="(message, index) of validationMessages['firstName']"
-          :key="index"
-        >
-          <div class="validation-message">{{ message }}</div>
-        </div>
-      </div>
-
-      <!-- Last Name Field -->
-      <div class="form-control">
-        <span class="p-float-label">
-          <InputText
-            type="text"
-            id="last-name"
-            v-model="lastName"
-            v-tooltip.bottom.focus="'Your name will be displayed publicly'"
-            :class="{
-              'p-invalid': validationMessages.hasOwnProperty('lastName')
-            }"
-            class="w-20rem "
-            @blur="sendSignUpInfo()"
-          />
-          <label for="last-name">Last Name</label>
-        </span>
-        <div
-          v-for="(message, index) of validationMessages['lastName']"
-          :key="index"
-        >
-          <div class="validation-message">{{ message }}</div>
-        </div>
+    <!-- First Name Field -->
+    <div class="form-control">
+      <span class="p-float-label">
+        <InputText
+          type="text"
+          id="first-name"
+          v-model="firstName"
+          v-tooltip.bottom.focus="'Your name will be displayed publicly'"
+          :class="{
+            'p-invalid': validationMessages.hasOwnProperty('firstName'),
+          }"
+          class="w-20rem "
+        />
+        <label for="first-name">First Name</label>
+      </span>
+      <div
+        v-for="(message, index) of validationMessages['firstName']"
+        :key="index"
+      >
+        <div class="validation-message">{{ message }}</div>
       </div>
     </div>
 
-    <div class="md:flex md:flex-row">
-      <!-- Email Field -->
-      <div class="form-control md:mr-3">
-        <span class="p-float-label">
-          <InputText
-            type="text"
-            id="email"
-            v-model="email"
-            :class="{
-              'p-invalid': validationMessages.hasOwnProperty('email')
-            }"
-            class="w-20rem "
-            @blur="sendSignUpInfo()"
-          />
-          <label for="email">Email</label>
-        </span>
-        <div
-          v-for="(message, index) of validationMessages['email']"
-          :key="index"
-        >
-          <div class="validation-message">{{ message }}</div>
-        </div>
-      </div>
-
-      <!-- City Field -->
-      <div class="form-control">
-        <span class="p-float-label">
-          <AutoComplete
-            forceSelection
-            type="text"
-            id="city"
-            :field="area => `${area.name}, ${area.region}, ${area.country}`"
-            v-model="area"
-            v-tooltip.bottom.focus="'Helps us plan events in your area'"
-            :suggestions="filteredCityOptions"
-            :minLength="3"
-            :delay="300"
-            @complete="searchCityOptions($event)"
-            :class="{
-              'p-invalid': validationMessages.hasOwnProperty('area')
-            }"
-            class="w-20rem "
-            @blur="sendSignUpInfo()"
-          />
-          <label for="city">City</label>
-        </span>
-        <div
-          v-for="(message, index) of validationMessages['area']"
-          :key="index"
-        >
-          <div class="validation-message">{{ message }}</div>
-        </div>
+    <!-- Last Name Field -->
+    <div class="form-control">
+      <span class="p-float-label">
+        <InputText
+          type="text"
+          id="last-name"
+          v-model="lastName"
+          v-tooltip.bottom.focus="'Your name will be displayed publicly'"
+          :class="{
+            'p-invalid': validationMessages.hasOwnProperty('lastName'),
+          }"
+          class="w-20rem "
+        />
+        <label for="last-name">Last Name</label>
+      </span>
+      <div
+        v-for="(message, index) of validationMessages['lastName']"
+        :key="index"
+      >
+        <div class="validation-message">{{ message }}</div>
       </div>
     </div>
 
-    <div class="md:flex md:flex-row">
-      <!-- Birthdate Field -->
-      <div class="form-control md:mr-3">
-        <span class="p-float-label">
-          <InputMask
-            v-model="birthdate"
-            v-tooltip.bottom.focus="'Helps us connect you with others your age'"
-            mask="99/99/9999"
-            slotChar="MM/DD/YYYY"
-            @change="sendSignUpInfo()"
-            :class="{
-              'p-invalid': validationMessages.hasOwnProperty('birthdate')
-            }"
-            class="w-20rem "
-          />
-          <label for="birthdate">Birth Date</label>
-        </span>
-        <div
-          v-for="(message, index) of validationMessages['birthdate']"
-          :key="index"
-        >
-          <div class="validation-message">{{ message }}</div>
-        </div>
-      </div>
-
-      <!-- Gender Field -->
-      <div class="form-control">
-        <span class="p-float-label">
-          <AutoComplete
-            type="text"
-            id="gender"
-            v-model="gender"
-            v-tooltip.bottom.focus="'Helps us facilitate certain events like women\'s soccer'"
-            :suggestions="filteredGenderOptions"
-            :minLength="1"
-            :delay="0"
-            @complete="searchGenderOptions($event)"
-            :class="{
-              'p-invalid': validationMessages.hasOwnProperty('gender')
-            }"
-            class="w-20rem "
-            @blur="sendSignUpInfo()"
-          />
-          <label for="gender">Gender (Optional)</label>
-        </span>
-        <div
-          v-for="(message, index) of validationMessages['gender']"
-          :key="index"
-        >
-          <div class="validation-message">{{ message }}</div>
-        </div>
+    <!-- Email Field -->
+    <div class="form-control ">
+      <span class="p-float-label">
+        <InputText
+          type="text"
+          id="email"
+          v-model="email"
+          :class="{
+            'p-invalid': validationMessages.hasOwnProperty('email'),
+          }"
+          class="w-20rem "
+          @input="$emit('send-email', email)"
+        />
+        <label for="email">Email</label>
+      </span>
+      <div v-for="(message, index) of validationMessages['email']" :key="index">
+        <div class="validation-message">{{ message }}</div>
       </div>
     </div>
+
+    <!-- City Field -->
+    <div class="form-control">
+      <span class="p-float-label">
+        <AutoComplete
+          forceSelection
+          type="text"
+          id="city"
+          :field="(area) => `${area.name}, ${area.region}, ${area.country}`"
+          v-model="area"
+          v-tooltip.bottom.focus="'Helps us plan events in your area'"
+          :suggestions="filteredCityOptions"
+          :minLength="3"
+          :delay="300"
+          @complete="searchCityOptions($event)"
+          :class="{
+            'p-invalid': validationMessages.hasOwnProperty('area'),
+          }"
+          class="w-20rem "
+        />
+        <label for="city">City</label>
+      </span>
+      <div v-for="(message, index) of validationMessages['area']" :key="index">
+        <div class="validation-message">{{ message }}</div>
+      </div>
+    </div>
+
+    <!-- Over 18 field -->
+    <div class="form-control">
+      <div class="field-checkbox mb-0 mt-2">
+        <Checkbox v-model="over18" id="over-18" :binary="true" />
+        <label for="over-18">I am 18 years old or over</label>
+      </div>
+      <div
+        v-for="(message, index) of validationMessages['over18']"
+        :key="index"
+      >
+        <div class="validation-message">{{ message }}</div>
+      </div>
+    </div>
+
+    <div class="mt-2">
+      <Button
+        label="Submit"
+        class="p-button-md p-button-primary submit-button"
+        @click="submitSignUp"
+      />
+    </div>
+    <Message
+      v-for="msg of serverResponses"
+      :severity="msg.severity"
+      :key="msg.content"
+    >
+      {{ msg.content }}
+    </Message>
   </div>
 </template>
 
 <script>
 import axios from "axios";
+import { signUpValidation } from "../../../resources/validation";
 
 export default {
   name: "Sign Up Form",
-  props: {
-    validationMessagesProp: Object
-  },
+  emits: ["next-step", "send-email"],
   data() {
     return {
+      serverResponses: [],
       firstName: "",
       lastName: "",
       email: "",
+      over18: false,
       filteredCityOptions: [],
       area: "",
-      gender: "",
-      birthdate: "",
-      filteredGenderOptions: [],
-      genderOptions: [
-        "Female",
-        "Male",
-        "Two spirit",
-        "Transgender",
-        "Non-binary",
-        "Agender",
-        "Genderqueer"
-      ],
-      validationMessages: this.validationMessagesProp
+      validationMessages: {},
     };
-  },
-  watch: {
-    validationMessagesProp: function(newVal) {
-      return (this.validationMessages = newVal);
-    }
-  },
-  computed: {
-    signUpInfoPayload() {
-      return {
-        firstName: this.firstName,
-        lastName: this.lastName,
-        email: this.email,
-        area: this.area,
-        gender: this.gender,
-        birthdate: this.birthdate
-      };
-    },
-    isMobile() {
-      return window.innerWidth <= 768;
-    }
   },
   methods: {
     searchCityOptions(city) {
       axios
         .post("/api/map/auto-complete", { input: city.query })
         .then(
-          res =>
+          (res) =>
             (this.filteredCityOptions = res.data.output.map(
               ({ name, region, country, coordinates }) => {
                 return {
                   name: name,
                   region: region,
                   country: country,
-                  location: { type: "Point", coordinates: coordinates }
+                  location: { type: "Point", coordinates: coordinates },
                 };
               }
             ))
         )
-        .catch(error => console.log("Error from /api/maps", error));
+        .catch((error) => console.log("Error from /api/maps", error));
     },
-    sendSignUpInfo() {
-      return this.$emit("send-sign-up-info", this.signUpInfoPayload);
+    submitSignUp() {
+      this.validationMessages = {};
+      const { error } = signUpValidation({
+        firstName: this.firstName,
+        lastName: this.lastName,
+        email: this.email,
+        area: this.area,
+        over18: this.over18,
+      });
+      if (error) {
+        error.details.forEach(({ path }) => {
+          let messages = error.details
+            .filter((val) => val.path[0] === path[0])
+            .map(({ message }) => message);
+          messages = [...new Set(messages)];
+          return (this.validationMessages[path[0]] = messages);
+        });
+        return;
+      }
+      axios
+        .post("/api/sign-up", {
+          firstName: this.firstName,
+          lastName: this.lastName,
+          email: this.email,
+          area: this.area,
+          over18: this.over18,
+        })
+        .then(() => {
+          this.$emit("next-step", "PreferencesForm");
+        })
+        .catch((error) =>
+          this.serverResponses.push({
+            severity: "error",
+            content: error.response.data.message,
+          })
+        );
     },
-    searchGenderOptions(event) {
-      return (this.filteredGenderOptions = this.genderOptions.filter(option => {
-        return option.toLowerCase().startsWith(event.query.toLowerCase());
-      }));
-    }
-  }
+  },
 };
 </script>
 
--- server/routes/signUp.js
+++ server/routes/signUp.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const Attendee = require("../models/Attendee.js");
-const mailer = require("../scripts/mailer.js");
 
-const { signUpValidation } = require("../../resources/validation");
+const {
+  signUpValidation,
+  preferencesValidation,
+} = require("../../resources/validation");
 
 const router = express.Router();
 
@@ -10,11 +12,13 @@ router.post("/", async (req, res) => {
   const { error } = signUpValidation(req.body);
   if (error) return res.status(400).json({ message: error.details[0].message });
 
-  const emailExists = await Attendee.findOne({ email: req.body.email })
+  const emailExists = await Attendee.findOne({ email: req.body.email });
 
   if (emailExists && emailExists.subscribed) {
     // Existing attendee who is already subscribed
-    return res.status(400).json({ message: "This email has already been registered" })
+    return res
+      .status(400)
+      .json({ message: "This email has already been registered" });
   } else if (emailExists && !emailExists.subscribed) {
     // Existing attendee who is not subscribed
     const attendee = {
@@ -22,32 +26,24 @@ router.post("/", async (req, res) => {
       lastName: req.body.lastName,
       email: req.body.email,
       area: req.body.area,
-      birthdate: req.body.birthdate,
-      gender: req.body.gender,
-      preferences: {
-        maxTravelDistance: req.body.maxTravelDistance,
-        interests: req.body.interests,
-        availability: req.body.availability,
-      },
-      selectedEventTags: req.body.selectedEventTags,
-      subscribed: true
-    }
+      subscribed: true,
+    };
 
     try {
-      const updatedAttendee = await Attendee.findOneAndUpdate({ email: req.body.email }, attendee)
-      res
-        .status(201)
-        .json({
-          code: 201,
-          message: "Existing attendee has been subscribed",
-          output: updatedAttendee,
-        });
+      const updatedAttendee = await Attendee.findOneAndUpdate(
+        { email: req.body.email },
+        attendee
+      );
+      res.status(201).json({
+        code: 201,
+        message: "Existing attendee has been subscribed",
+        output: updatedAttendee,
+      });
     } catch (error) {
       return res
         .status(400)
         .json({ code: error.code, message: error.message, stack: error.stack });
     }
-
   } else {
     // New attendee
     const attendee = new Attendee({
@@ -55,31 +51,47 @@ router.post("/", async (req, res) => {
       lastName: req.body.lastName,
       email: req.body.email,
       area: req.body.area,
-      birthdate: req.body.birthdate,
-      gender: req.body.gender,
-      preferences: {
-        maxTravelDistance: req.body.maxTravelDistance,
-        interests: req.body.interests,
-        availability: req.body.availability,
-      },
-      selectedEventTags: req.body.selectedEventTags,
-      subscribed: true
+      subscribed: true,
     });
 
     try {
       const savedAttendee = await attendee.save();
-      res
-        .status(201)
-        .json({
-          code: 201,
-          message: "attendee saved successfully",
-          output: savedAttendee,
-        });
+      res.status(201).json({
+        code: 201,
+        message: "attendee saved successfully",
+        output: savedAttendee,
+      });
     } catch (error) {
       return res
         .status(400)
         .json({ code: error.code, message: error.message, stack: error.stack });
     }
+  }
+});
+
+router.put("/preferences", async (req, res) => {
+  const { error } = preferencesValidation(req.body.preferences);
+  if (error) return res.status(400).json({ message: error.details[0].message });
+
+  const emailExists = await Attendee.findOne({ email: req.body.email });
+  if (!emailExists) {
+    return res.status(400).json({ message: "This email is not signed up" });
+  }
+
+  try {
+    const updatedAttendee = await Attendee.findOneAndUpdate(
+      { email: req.body.email },
+      req.body.preferences
+    );
+    res.status(201).json({
+      code: 201,
+      message: "Attendee's preferences have been updated",
+      output: updatedAttendee,
+    });
+  } catch (error) {
+    return res
+      .status(400)
+      .json({ code: error.code, message: error.message, stack: error.stack });
   }
 });
 
