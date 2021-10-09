b11a111a2aea302dc720e04cd632fc9407bf6bca
Shareef Hadid <shareefhadid@gmail.com>
2021-10-09 13:20:53 -0400

    fixed 404

--- client/src/views/404.vue
+++ client/src/views/404.vue
@@ -1,17 +1,17 @@
 <template>
   <section class="section off-white-bg">
-    <div class="container page-not-found-container">
-      <div class="mx-3 ">
-        <h1>404: Page Not Found</h1>
+    <div
+      class="container page-not-found-container flex flex-column text-center justify-content-center align-items-center mx-3"
+    >
+      <h1>404: Page Not Found</h1>
 
-        <h2>
-          If you have any questions, you can reach us at
-          <a href="mailto:team@humeone.com">team@humeone.com</a>
-        </h2>
+      <h2>
+        If you have any questions, you can reach us at
+        <a href="mailto:team@humeone.com">team@humeone.com</a>
+      </h2>
 
-        <div class="cta-button mt-5 border-round" @click="routeToHome()">
-          Back to Home
-        </div>
+      <div class="cta-button mt-5 border-round" @click="routeToHome()">
+        Back to Home
       </div>
     </div>
   </section>
@@ -39,11 +39,8 @@ export default {
 <style scoped lang="scss">
 .page-not-found-container {
   color: #444450;
-  display: flex;
-  flex-direction: column;
   padding: 30vh 0;
   margin-bottom: 32px;
-  text-align: center;
   h1 {
     font-size: 32px;
     font-weight: 600;
