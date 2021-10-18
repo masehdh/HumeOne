0d0762935caeaa7cce798c50f8864a09b1b16a3a
Shareef Hadid <shareefhadid@gmail.com>
2021-10-18 16:54:31 -0400

    moved social share to component

---
+++ client/src/components/SocialShareIcons.vue
@@ -0,0 +1,91 @@
+<template>
+  <div class="flex flex-row align-items-center mt-3 md:mt-0">
+    <a @click="copyUrl()" class="relative mr-2">
+      <font-awesome-icon :icon="['fas', 'link']" class="copy-icon" />
+      <p
+        v-show="showCopyTip"
+        class="copy-tip text-sm select-none ml-1 py-1 px-1 absolute left-100 border-round z-1"
+        style="background-color: rgb(50,50,50); color: white;"
+      >
+        Copied!
+      </p>
+    </a>
+    <a
+      :href="shareLink.facebook"
+      target="_blank"
+      @click.prevent="showShareWindow(shareLink.facebook)"
+      class="mr-2"
+    >
+      <font-awesome-icon :icon="['fab', 'facebook-square']" />
+    </a>
+
+    <a
+      :href="shareLink.twitter"
+      target="_blank"
+      @click.prevent="showShareWindow(shareLink.twitter)"
+      class="mr-2"
+    >
+      <font-awesome-icon :icon="['fab', 'twitter-square']" />
+    </a>
+
+    <a
+      :href="shareLink.linkedin"
+      @click.prevent="showShareWindow(shareLink.linkedin)"
+      target="_blank"
+    >
+      <font-awesome-icon :icon="['fab', 'linkedin']" />
+    </a>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Social Share Icons",
+  props: {
+    eventLink: String,
+    eventDetails: Object
+  },
+  data() {
+    return {
+      showCopyTip: false
+    };
+  },
+  computed: {
+    shareLink() {
+      return {
+        facebook: `https://www.facebook.com/dialog/share?app_id=431777641641190&href=${this.eventLink}&quote=HumeOne is hosting "${this.eventDetails.name}" on ${this.eventDetails.dateTime}. Check it out using the link below.&hashtag=%23HumeOne`,
+        twitter: `https://twitter.com/intent/tweet?url=${this.eventLink}&text=HumeOne is hosting "${this.eventDetails.name}" on ${this.eventDetails.dateTime}. Check it out using the link below.&hashtags=HumeOne,ThingsToDo,Events`,
+        linkedin: `https://www.linkedin.com/sharing/share-offsite/?url=${this.eventLink}`
+      };
+    }
+  },
+  methods: {
+    showShareWindow(value) {
+      let left = (screen.width - 570) / 2;
+      let top = (screen.height - 570) / 2;
+      let params =
+        "menubar=no,toolbar=no,status=no,width=570,height=570,top=" +
+        top +
+        ",left=" +
+        left;
+
+      const url = encodeURI(value);
+
+      window.open(url, "NewWindow", params);
+    },
+    copyUrl() {
+      navigator.clipboard.writeText(this.eventLink);
+      this.showCopyTip = true;
+      setTimeout(() => {
+        this.showCopyTip = false;
+      }, 1000);
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.copy-tip {
+  top: -2px;
+}
+</style>
--- client/src/views/EventRegistrationConfirmation.vue
+++ client/src/views/EventRegistrationConfirmation.vue
@@ -1,25 +1,37 @@
 <template>
   <Head>
-    <meta name="description" :content="eventDetails.description" />
+    <meta name="robots" content="noindex" />
+    <meta
+      name="description"
+      v-if="eventId"
+      :content="eventDetails.description"
+    />
 
     <!-- Open Graph for Social Share -->
-    <meta property="og:title" :content="eventDetails.name" />
+    <meta property="og:title" v-if="eventId" :content="eventDetails.name" />
     <meta
       property="og:image"
       :content="
         'https://humeone.com' + require(`@/assets/${eventDetails.image}`)
       "
+      v-if="eventId"
     />
-    <meta property="og:description" :content="eventDetails.description" />
-    <meta property="og:url" :content="eventLink" />
+    <meta
+      property="og:description"
+      v-if="eventId"
+      :content="eventDetails.description"
+    />
+    <meta property="og:url" v-if="eventId" :content="eventLink" />
   </Head>
 
   <section class="section form-section">
     <div class="container form-card px-3 py-4 md:px-4 md:py-5">
       <h1 class="form-header">Registration Confirmation</h1>
+
       <h2 class="form-title" v-if="eventDetails.name">
         {{ eventDetails.name }}
       </h2>
+
       <p class="line-height-3 mt-3">
         Thank you for registering! You should receive a confirmation email
         shortly with additional details. If you have any questions or would like
@@ -28,51 +40,15 @@
       </p>
 
       <div
-        class="flex flex-column md:flex-row-reverse md:align-items-baseline justify-content-center md:justify-content-between"
+        class="flex flex-column-reverse md:flex-row md:align-items-baseline justify-content-center md:justify-content-between"
       >
-        <div class="flex flex-row align-items-center mt-3 md:mt-0">
-          <a @click="copyUrl()" class="relative mr-2">
-            <font-awesome-icon :icon="['fas', 'link']" class="copy-icon" />
-            <p
-              v-show="showCopyTip"
-              class="copy-tip text-sm select-none ml-1 py-1 px-1 absolute left-100 border-round z-1"
-              style="background-color: rgb(50,50,50); color: white;"
-            >
-              Copied!
-            </p>
-          </a>
-          <a
-            :href="shareLink.facebook"
-            target="_blank"
-            @click.prevent="showShareWindow(shareLink.facebook)"
-            class="mr-2"
-          >
-            <font-awesome-icon :icon="['fab', 'facebook-square']" />
-          </a>
-
-          <a
-            :href="shareLink.twitter"
-            target="_blank"
-            @click.prevent="showShareWindow(shareLink.twitter)"
-            class="mr-2"
-          >
-            <font-awesome-icon :icon="['fab', 'twitter-square']" />
-          </a>
-
-          <a
-            :href="shareLink.linkedin"
-            @click.prevent="showShareWindow(shareLink.linkedin)"
-            target="_blank"
-          >
-            <font-awesome-icon :icon="['fab', 'linkedin']" />
-          </a>
-        </div>
-
         <a href="/" @click.prevent="routeToHome()" class="no-underline mt-3">
           <div class="cta-button border-round">
             Back to Home
           </div>
         </a>
+
+        <SocialShareIcons :eventDetails="eventDetails" :eventLink="eventLink" />
       </div>
     </div>
   </section>
@@ -80,59 +56,36 @@
 
 <script>
 import eventList from "../../../resources/events.json";
+import SocialShareIcons from "@/components/SocialShareIcons";
 
 export default {
   name: "Event Registration Confirmation",
+  components: { SocialShareIcons },
   data() {
     return {
-      showCopyTip: false,
       eventDetails:
         eventList.find(event => event.id === this.$route.query.eventId) || {},
       eventId: this.$route.query.eventId
     };
   },
+  created() {
+    if (!this.$route.query.eventId) this.$router.push({ name: "404" });
+  },
   computed: {
     eventLink() {
       return `https://www.humeone.com/event-registration?eventId=${this.eventId}`;
-    },
-    shareLink() {
-      return {
-        facebook: `https://www.facebook.com/dialog/share?app_id=431777641641190&href=${this.eventLink}&quote=HumeOne is hosting "${this.eventDetails.name}" on ${this.eventDetails.dateTime}. Check it out using the link below.&hashtag=%23HumeOne`,
-        twitter: `https://twitter.com/intent/tweet?url=${this.eventLink}&text=HumeOne is hosting "${this.eventDetails.name}" on ${this.eventDetails.dateTime}. Check it out using the link below.&hashtags=HumeOne,ThingsToDo,Events`,
-        linkedin: `https://www.linkedin.com/sharing/share-offsite/?url=${this.eventLink}`
-      };
     }
   },
   methods: {
     routeToHome() {
       this.$router.push({ name: "Home" });
-    },
-    showShareWindow(value) {
-      let left = (screen.width - 570) / 2;
-      let top = (screen.height - 570) / 2;
-      let params =
-        "menubar=no,toolbar=no,status=no,width=570,height=570,top=" +
-        top +
-        ",left=" +
-        left;
-
-      const url = encodeURI(value);
-
-      window.open(url, "NewWindow", params);
-    },
-    copyUrl() {
-      navigator.clipboard.writeText(this.eventLink);
-      this.showCopyTip = true;
-      setTimeout(() => {
-        this.showCopyTip = false;
-      }, 1000);
     }
   }
 };
 </script>
 
 <style scoped lang="scss">
-.svg-inline--fa {
+:deep(.svg-inline--fa) {
   color: $primary-font-color;
   transition: all 0.2s;
   font-size: 28px;
@@ -145,11 +98,7 @@ export default {
   }
 }
 
-.copy-icon {
+:deep(.copy-icon) {
   font-size: 24px;
-}
-
-.copy-tip {
-  top: -2px;
 }
 </style>
