fc9ef923b8bcc17e9a4a8dd3cb5ef4370243f9dc
Shareef Hadid <shareefhadid@gmail.com>
2021-10-19 13:13:16 -0400

    fixed upcoming events

--- client/src/views/UpcomingEvents.vue
+++ client/src/views/UpcomingEvents.vue
@@ -1,31 +1,57 @@
 <template>
+  <Head>
+    <meta
+      name="description"
+      content="View upcoming events from HumeOne."
+    />
+  </Head>
+
   <div class="section off-white-bg min-h-screen">
-    <div class="container">
+    <div class="container px-3 md:px-4">
       <h2 class="primary-font-color block text-center text-3xl mt-6 font-bold">
         Upcoming Events
       </h2>
 
       <div class="accent-divider w-6rem text-center ml-auto mr-auto mt-3"></div>
 
-      <div
-        class="flex flex-column justify-content-evenly  align-items-center md:flex-row w-full mt-6"
-      >
+      <div class="grid mt-5">
         <template v-for="(event, index) in upcomingEvents" :key="index">
-          <div
-            class="upcoming-event-card inline flex flex-column justify-content-end align-items-center text-center h-20rem w-10 md:w-15rem px-2 py-2 border-round mx-1 mb-4 md:mb-0"
-            :style="{
-              backgroundImage:
-                'linear-gradient(rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.9)), url(' +
-                require(`@/assets/${event.image}`) +
-                ')'
-            }"
-            @click="showEventModal(index)"
-          >
-            <p class="text-base">{{ event.name }}</p>
-            <p class="mt-1 text-sm">@{{ event.location }}</p>
-            <p class="mt-1 text-xs">{{ event.area }}</p>
+          <div class="col-12 md:col-4 px-2">
+            <div
+              class="upcoming-event-card flex flex-column justify-content-end align-items-center text-center h-20rem px-2 py-2 border-round mb-4 md:mb-0"
+              :style="{
+                backgroundImage:
+                  'linear-gradient(rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.9)), url(' +
+                  require(`@/assets/${event.image}`) +
+                  ')'
+              }"
+              @click="showEventModal(index)"
+            >
+              <p class="text-base">{{ event.name }}</p>
+              <p class="mt-1 text-sm">@{{ event.location }}</p>
+              <p class="mt-1 text-xs">{{ event.area }}</p>
+            </div>
           </div>
         </template>
+      </div>
+
+      <div
+        class="flex justify-content-center align-items-center text-center flex-column"
+      >
+        <p class="text-lg mt-3 md:mt-5 text-base">
+          Don't want to miss out? Sign up today and let us know what events you
+          want in your area.
+        </p>
+
+        <a
+          href="/sign-up"
+          @click.prevent="routeToSignUp()"
+          class="no-underline mt-4"
+        >
+          <div class="cta-button border-pill">
+            SIGN UP FOR FREE
+          </div>
+        </a>
       </div>
 
       <Dialog
@@ -141,6 +167,9 @@ export default {
         name: "Event Registration",
         query: { eventId: id }
       });
+    },
+    routeToSignUp() {
+      return this.$router.push({ name: "Sign Up" });
     }
   }
 };
